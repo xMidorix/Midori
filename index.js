879a52c93b82165c9e8addffb9dd2c3a190c3622
Midori <84060730+xMidorix@users.noreply.github.com>
2022-06-17 11:58:13 +0545

    Update index.js
    + Minor changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const aoijs = require("aoi.js")
 var fs = require('fs')
 
 const bot = new aoijs.Bot({
-  token: "OTE5OTM5NzgzMjE0NjQ5Mzg0.Gs5C7H.tJJKpScXFWp_IaR84kMATS9_GUnMYJCTZL2uTA",
-  prefix: "-",
+  token: "BOT_TOKEN",
+  prefix: "BOT_PREFIX",
   intents: ["GUILDS", "GUILD_MESSAGES"]
 })
 
@@ -47,7 +47,7 @@ bot.command({
 
 
 bot.readyCommand({
-  channel: "987017322193649726",
+  channel: "channel_is",
   code: `$title[1;Reboot]
 $thumbnail[1;https://media.discordapp.net/attachments/985530272202260531/986976892022300752/midori.png?width=113&height=113]
 $description[1;Midori was recently restarted 
@@ -55,8 +55,6 @@ $description[1;Midori was recently restarted
 Initialized on aoi.js v5.2.0
 
 
-
-[𝙐𝙬𝙐 Click Me](https://youtu.be/xjrU3N8M4eo?t=11)]
 $image[1;https://lh3.googleusercontent.com/yaaT5_QSTcSQdtdY5KXjLCs6raBxkhMMGWfKSleJcuiwleGXPXhxEKYQXmAf_LN3l5eyI_P-TaEQEyLPjjbX6SQ0h3B7AFhQyTPXLQ=w1400-k]
 $footer[1;]$addTimestamp[1;]
 $color[1;#fe2c54]`
